570558f4a95e16762c11524c4a617a3ce88cf341
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:25:59 +0200

    created example of code

--- SeaportFacilities/code/code_for_using_dataModel.Ports_SeaportFacilities.py
+++ SeaportFacilities/code/code_for_using_dataModel.Ports_SeaportFacilities.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "SeaportFacilities"
 subject = "dataModel.Ports"
-dateLastReported = "{'type': 'Property', 'value': '2020-03-17T08:45:00Z', 'metadata': {'TimeInstant': {'type': 'Text', 'value': '2020-03-17TT08:45:00Z'}}}"
+dateLastReported = "2020-03-17T08:45:00Z"
 attribute = "dateLastReported"
 value = dateLastReported
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refBoatAuthorized = {'type': 'Relationship', 'value': ['urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-yatching', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-passenger', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-fishing', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-cargo', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-tankers', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-specialist', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-war', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-historic']}
+refBoatAuthorized = ['urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-yatching', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-passenger', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-fishing', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-cargo', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-tankers', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-specialist', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-war', 'urn:ngsi-ld:BoatAuthorized:MNCA-NCE-BA-001-historic']
 attribute = "refBoatAuthorized"
 value = refBoatAuthorized
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refBoatPlaceAvailable = {'type': 'Relationship', 'value': ['urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-A', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-BC', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-DE', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-FG', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-HI', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-JK', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-LO', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-PQ', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-RT2', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-RT2', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-U', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-VW', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-XZ', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-Z02']}
+refBoatPlaceAvailable = ['urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-A', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-BC', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-DE', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-FG', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-HI', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-JK', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-LO', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-PQ', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-RT2', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-RT2', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-U', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-VW', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-XZ', 'urn:ngsi-ld:BoatPlaceAvailable:MNCA-BPA-Range-Z02']
 attribute = "refBoatPlaceAvailable"
 value = refBoatPlaceAvailable
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-refBoatPlacePricing = {'type': 'Relationship', 'value': ['urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-A', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-BC', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-DE', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-FG', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-HI', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-JK', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-LO', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-PQ', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-RT2', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-RT2', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-U', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-VW', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-XZ', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-Z02']}
+refBoatPlacePricing = ['urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-A', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-BC', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-DE', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-FG', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-HI', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-JK', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-LO', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-PQ', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-RT2', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-RT2', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-U', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-VW', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-XZ', 'urn:ngsi-ld:BoatPlacePricing:MNCA-BPP-Range-Z02']
 attribute = "refBoatPlacePricing"
 value = refBoatPlacePricing
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
